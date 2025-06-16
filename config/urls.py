fefcbab3ba74fe1e50375ae2d8fb84f78fb632fe
majed1985 <eng.majed.1985@gmail.com>
2025-06-16 17:28:29 +0300

    Add files via upload

---
+++ config/urls.py
@@ -0,0 +1,15 @@
+# config/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls', namespace='core')),  # تضمين مسارات تطبيق core
+]
+
+# خدمة ملفات static و media أثناء التطوير
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ core/admin.py
@@ -0,0 +1,40 @@
+# core/admin.py
+from django.contrib import admin
+
+from .models import Learner, Department, Section, Nationality, Sector
+
+
+@admin.register(Learner)
+class LearnerAdmin(admin.ModelAdmin):
+    list_display = ("first_name_ar", "last_name_ar", "date_created")
+    search_fields = (
+        "first_name_ar",
+        "last_name_ar",
+        "first_name_en",
+        "last_name_en",
+    )
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ("department", "name")
+    list_filter = ("department",)
+    search_fields = ("name",)
+
+
+@admin.register(Nationality)
+class NationalityAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Sector)
+class SectorAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
