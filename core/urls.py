cf2a5d3a5b9cf5d15874a873030fc80cb42af4cd
majed1985 <eng.majed.1985@gmail.com>
2025-06-18 16:49:44 +0300

    Fix SyntaxError in core.urls

--- core/urls.py
+++ core/urls.py
@@ -64,4 +64,4 @@ urlpatterns = [
     path("recruitment/<int:pk>/evaluate/", views.evaluate_employee, name="evaluate_employee"),
     path("recruitment/input-results/", views.input_evaluation_results, name="input_evaluation_results"),
     path("recruitment/<int:pk>/final-score/", views.set_final_score, name="set_final_score"),
-
+]
