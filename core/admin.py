2a76cc119953e2da45ddaa3cb600c177e2b57a53
majed1985 <eng.majed.1985@gmail.com>
2025-07-12 03:44:19 +0300

    Remove Learner model from admin

--- core/admin.py
+++ core/admin.py
@@ -1,20 +1,9 @@
 # core/admin.py
 from django.contrib import admin
 
-from .models import Learner
-
-
-@admin.register(Learner)
-class LearnerAdmin(admin.ModelAdmin):
-    list_display = ("first_name_ar", "last_name_ar", "date_created")
-    search_fields = (
-        "first_name_ar",
-        "last_name_ar",
-        "first_name_en",
-        "last_name_en",
-    )
-
-
+# The Learner model is intentionally not registered with the Django admin
+# interface. Administrators manage trainees through custom views instead of
+# using the default admin site.
 # The following models were previously registered with the admin site but have
 # been removed as they are no longer managed through the Django admin
 # interface:
@@ -26,4 +15,3 @@ class LearnerAdmin(admin.ModelAdmin):
 # - EmployeeEvaluation
 # - RecruitmentReport
 # - LegacyRecruitmentRecord
-
